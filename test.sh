fbcbdce1e335c7bad9f6d95f2190b28284df3d76
Iosif <dev@iosifv.com>
2025-02-19 17:55:00 +0000

    [main] - try again

--- test.sh
+++ test.sh
@@ -1,10 +1,5 @@
 # npm install jsonresume-theme-stackoverflow@2.0
 # npm install jsonresume-theme-caffeine
 
-# npm install jsonresume-theme-paper
-# npm install jsonresume-theme-kendall
-
 resume export exports/stackoverflow.pdf     --theme stackoverflow
 resume export exports/macchiato.pdf         --theme macchiato
-resume export exports/paper.pdf             --theme paper
-resume export exports/kendall.pdf           --theme kendall
